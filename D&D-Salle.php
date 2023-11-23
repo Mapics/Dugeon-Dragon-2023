7ea7c890e514a081c7664f2df0c53b7593907607
ttnmeoeayhrnefr <theo.menantferry@outlook.com>
2023-11-23 14:32:56 +0100

    Update D&D-Salle.php
    Ajouts Salle.php

--- D&D-Salle.php
+++ D&D-Salle.php
@@ -19,6 +19,10 @@ abstract class Salle
 }
 class SalleVide extends Salle
 {
+    public function __construct($type, $description)
+    {
+        parent::__construct($type, $description);
+    }
 }
 class SalleCombat extends Salle
 {
@@ -31,5 +35,31 @@ class SalleCombat extends Salle
     public function getMonstre()
     {
         return $this->monstre;
+    }
+}
+class SalleBoss extends Salle
+{
+    protected $boss;
+    public function __construct($type, $description, $boss)
+    {
+        parent::__construct($type, $description);
+        $this->boss = $boss;
+    }
+    public function getBoss()
+    {
+        return $this->boss;
+    }
+}
+class SalleEnigme extends Salle
+{
+    protected $enigme;
+    public function __construct($type, $description, $enigme)
+    {
+        parent::__construct($type, $description);
+        $this->enigme = $enigme;
+    }
+    public function getEnigme()
+    {
+        return $this->enigme;
     }
 }
