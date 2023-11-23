8e7079098ebcc1fdd1edfe3e9d5ac621e896f0e5
ttnmeoeayhrnefr <theo.menantferry@outlook.com>
2023-11-23 15:56:39 +0100

    Update D&D-DAO.php
    CRUD Arme

--- D&D-DAO.php
+++ D&D-DAO.php
@@ -1,10 +1,40 @@
 <?php
-class ddDAO {
+
+class ddDAO
+{
     protected $bdd;
 
-    public function __construct($bdd) {
+    public function __construct($bdd)
+    {
         $this->bdd = $bdd;
     }
-}
 
-?>
+    public function ajouterArme($nom, $degat, $niv_requis)
+    {
+        $query = "INSERT INTO armes (Nom_arme, Degat, Niv_requis) VALUES (?, ?, ?)";
+        $stmt = $this->bdd->prepare($query);
+        $stmt->execute([$nom, $degat, $niv_requis]);
+    }
+
+    public function getArmeById($id)
+    {
+        $query = "SELECT * FROM armes WHERE Id_arme = ?";
+        $stmt = $this->bdd->prepare($query);
+        $stmt->execute([$id]);
+        return $stmt->fetch();
+    }
+
+    public function modifierArme($id, $nom, $degat, $niv_requis)
+    {
+        $query = "UPDATE armes SET Nom_arme = ?, Degat = ?, Niv_requis = ? WHERE Id_arme = ?";
+        $stmt = $this->bdd->prepare($query);
+        $stmt->execute([$nom, $degat, $niv_requis, $id]);
+    }
+
+    public function supprimerArme($id)
+    {
+        $query = "DELETE FROM armes WHERE Id_arme = ?";
+        $stmt = $this->bdd->prepare($query);
+        $stmt->execute([$id]);
+    }
+}
