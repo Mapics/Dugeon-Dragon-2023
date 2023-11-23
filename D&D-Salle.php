329c4b23a2bead62e07256c181d2a4db375cdae6
ttnmeoeayhrnefr <theo.menantferry@outlook.com>
2023-11-23 12:20:18 +0100

    Update D&D-Salle.php
    Ajouts Salle.php

--- D&D-Salle.php
+++ D&D-Salle.php
@@ -1,15 +1,54 @@
 <?php
-class Salle{
-    protected $id;
-    protected $monstre;
-    protected $piege;
-    protected $marchand;
 
-    function __construct($id) {
-        $this->id = $id;
-        $this->monstre = false;
-        $this->piege = false;
-        $this->marchand = false;
+abstract class Salle
+{
+    protected $type;
+    protected $description;
+
+    public function __construct($type, $description)
+    {
+        $this->type = $type;
+        $this->description = $description;
+    }
+
+    public function getType()
+    {
+        return $this->type;
+    }
+
+    public function getDescription()
+    {
+        return $this->description;
+    }
+
+    abstract public function afficherDescription();
+}
+
+class SalleVide extends Salle
+{
+    public function afficherDescription()
+    {
+        return "Vous entrez dans une salle vide. Rien ne s'y trouve.";
     }
 }
-?>
+
+class SalleCombat extends Salle
+{
+    protected $monstre;
+
+    public function __construct($type, $description, $monstre)
+    {
+        parent::__construct($type, $description);
+        $this->monstre = $monstre;
+    }
+
+    public function getMonstre()
+    {
+        return $this->monstre;
+    }
+
+    public function afficherDescription()
+    {
+        return "Vous entrez dans une salle de combat. Un " . $this->monstre . " vous attaque !";
+    }
+}
