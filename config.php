510cacc012b0345cb3c7667ee7b86739b3470d93
Purplezer <a.do@ecole-ipssi.net>
2023-11-24 16:42:57 +0100

    Update config.php

--- config.php
+++ config.php
@@ -2,8 +2,8 @@
 // PARAMETRES DE CONNEXION A LA BASE DE DONNEES POUR NE PAS APPARAITRE EN BRUT DANS LE FICHIER D'ORIGINE
 $config = array(
     'hote' => 'localhost',
-    'utilisateur' => 'dd_root',
-    'motDePasse' => 'password',
+    'utilisateur' => 'ddprojet',
+    'motDePasse' => 'projet',
     'nomDeLaBase' => 'ddprojet',
     'port' => '3306',
 );
