e70c2c96a57a5c023a4acf9945c278b5b39e88b7
ttnmeoeayhrnefr <theo.menantferry@outlook.com>
2023-11-24 15:36:39 +0100

    Update D&D.php
    Commentaires

--- D&D.php
+++ D&D.php
@@ -1,11 +1,19 @@
 <?php
+// FICHIERS A IMPORTER
 require_once('config.php');
 require_once('D&D-DAO.php');
 require_once('D&D-Game.php');
 require_once('D&D-Personnage.php');
 require_once('D&D-Salle.php');
+
+// MISE EN PLACE DE LA CONNEXION A LA BASE DE DONNEES
 $connexion = new PDO("mysql:host={$config['hote']};port={$config['port']};dbname={$config['nomDeLaBase']}", $config['utilisateur'], $config['motDePasse']);
 
+// CONNEXION A LA BASE DE DONNEES
 $ddDAO = new DD_DAO($connexion);
+
+// CREER LE JEU
 $ddGame = new DD_Game($ddDAO);
+
+// DEMARRE LA PARTIE
 $ddGame->initGame($ddDAO);
