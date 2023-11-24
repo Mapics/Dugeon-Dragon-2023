b55606aee713d8af0974c70d6d1a539b1952c988
ttnmeoeayhrnefr <theo.menantferry@outlook.com>
2023-11-24 15:38:18 +0100

    Update config.php
    Commentaires

--- config.php
+++ config.php
@@ -1,4 +1,5 @@
 <?php
+// PARAMETRES DE CONNEXION A LA BASE DE DONNEES POUR NE PAS APPARAITRE EN BRUT DANS LE FICHIER D'ORIGINE
 $config = array(
     'hote' => 'localhost',
     'utilisateur' => 'dd_root',
